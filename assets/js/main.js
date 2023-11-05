77777eb3a1748849dd63d31a533b257e0fec0987
abdul-aziz <tamimaziz2007@gmail.com>
2023-11-05 16:24:11 +0600

    responsive file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,31 @@
 $(document).ready(function () {
-          var swiper = new Swiper(".mySwiper", {
-            slidesPerView: 3,
-            spaceBetween: 30,
-            pagination: {
-              el: ".swiper-pagination",
-              clickable: true,
-            },
-            navigation: {
-                nextEl: ".swiper-button-next",
-                prevEl: ".swiper-button-prev",
-              },
-              breakpoints: {  
-                '767': {
-                    slidesPerView: 3,
-                    spaceBetween: 20,
-                },
-                '480': {
-                  slidesPerView: 2,
-                  spaceBetween: 20,},
-                '@0': {
-                  slidesPerView: 1,
-                  spaceBetween: 10, },
-              },
-          });
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      767: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      480: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 20,},
+    },
+  });
 });
